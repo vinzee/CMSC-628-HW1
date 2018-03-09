ec203b1c1eb188f208a9ed8d75a268959160674c
Vineet Ahirkar <vinzee93@gmail.com>
2018-03-08 20:25:08 -0500

    first commit

---
+++ app/src/main/java/com/example/vinzee/cmsc_628_hw1/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.vinzee.cmsc_628_hw1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
